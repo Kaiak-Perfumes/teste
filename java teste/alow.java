e11db2f7c334c9623ff4ea9cb78373d720200ed5
Kaiak-Perfumes <69114210+Kaiak-Perfumes@users.noreply.github.com>
2020-09-11 02:27:35 -0300

    teste - commit
    basic login program based on java

---
+++ java teste/alow.java
@@ -0,0 +1,30 @@
+//teste 
+import java.util.Scanner;
+public class alow {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Olá, qual o seu login? ");
+        Scanner y = new Scanner(System.in);
+        String teste = y.next();
+        System.out.println("Qual sua senha? ");
+        Scanner z = new Scanner(System.in);
+        String pass = z.next();
+        
+        if (teste.equals ("kaianmesquita119") && pass.equals ("K87511822k")){
+            System.out.println("Bem-vindo, " + teste);
+            System.out.println("Deseja acessar configurações da conta?");
+            String acesso = y.next();
+            if(acesso.equals("sim")){
+                System.out.println("Então vai toma no cukkkkk");
+            }else{
+                System.out.println("GAYKKKKK");
+            
+            }
+        }else{
+            System.out.println("Login ou senha inválidos. Tente novamente.");
+        }
+            
+        
+    }
+}
